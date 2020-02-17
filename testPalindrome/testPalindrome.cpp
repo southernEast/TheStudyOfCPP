9c464d5a63e66ce1e04037de5b06fe252b0bb9ae
shadow <773850645@qq.com>
2020-02-18 02:32:59 +0800

    add homework file

---
+++ testPalindrome/testPalindrome.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream> 
+#include <algorithm>
+#include <string>
+using namespace std;
+
+bool noSym(char ch) {
+	if (ch == '.' || ch == ',' || ch == '!'
+		|| ch == ';' || ch == ':' || ch == '('
+		|| ch == ')')
+		return false;
+	else
+		return true;
+}
+
+bool testPalindrome(string str) {
+	string noSymStr, reStr;
+	copy_if(str.begin(), str.end(), back_inserter(noSymStr), noSym);
+	/*for (auto elem : noSymStr)
+		cout << elem;*/
+	reStr = noSymStr;
+	reverse(reStr.begin(), reStr.end());
+	for (int i = 0; i < noSymStr.size(); ++i) {
+		if (noSymStr[i] != reStr[i])
+			return false;
+	}
+	return true;
+}
+
+int main() {
+	string str;
+	getline(cin, str);
+	cout << boolalpha << testPalindrome(str);
+
+	return 0;
+}
